7187a63d07050a0a790199f53b5fd1fe925af0cf
Александра <shunya@mail.ru>
2024-05-25 20:48:18 +0300

    final editing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
 
+// Include Lightbox 
+import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  // may select multiple "galleries"
+  gallery: '#gallery--getting-started',
+
+  // Elements within gallery (slides)
+  children: 'a',
+
+  // setup PhotoSwipe Core dynamic import
+  pswpModule: () => import('/photoswipe/photoswipe.esm.js')
+});
+lightbox.init();
 
 
 async function sendForm(form) {
